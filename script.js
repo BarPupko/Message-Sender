7e9b81ff3815ff68ad49a0316619e1b670db65b0
Bar Pupko <Barpupco@gmail.com>
2022-08-29 10:13:21 +0300

    update copyright

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function updateNumbers(){
-    let userInput = document.getElementById("new-bio").value;
+    let userInput = document.getElementById("new-bio").value; //getting the output from textarea
+    let userTextInput = document.getElementById("msg").value; //getting the text
+    // console.log("usertext:"+userTextInput);
    
     var lines = userInput.split(/\r|\r\n|\n/);
-    console.log("lines:"+lines);
+    // console.log("lines:"+lines);
     var count = lines.length;
-    console.log("row:" + count); // Outputs 4
+    // console.log("row:" + count); // Outputs 4
 
 
     //creating elements with numbers
@@ -17,15 +19,26 @@ function updateNumbers(){
                 el=el.replace('-','');//remove break between number
             }
 
-        link = `"https://wa.me/${el}"\n`//generate link
+        link = `"https://wa.me/${el}?text=${userTextInput}"\n`//generate link
         // link = `"https://api.whatsapp.com/send/?phone=%2B972${el}"\n`//generate link
         let blank = target ="_blank";
         let rel = "noreferrer noopener";
-        console.log("lines: " +lines);
-        console.log("el: " +el);
+        // console.log("lines: " +lines);
+        // console.log("el: " +el);
+
+        //  **** new feature still under development. ****
+        //open new window automaticlly and send the text into this window
+             // div.innerHTML = `\n<a href="${win1}"a>"${el}"</a>\n`;
+        // div.innerHTML = `\n<a href="${win}"a>"${el}"</a>\n`;
+        // div.innerHTML = `<p>${win}</p>`;
         
+
+        //working method
         div.innerHTML = "\n<a href="+link + blank + rel +"a>"+el+"</a>\n";
+  
         document.body.appendChild(div);
+
+        console.log("copyright bar popko,search me on github")
     });
 
  
