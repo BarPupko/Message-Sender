f3d37ee4e6bbc3c5a75485d75e74b2d4b4feab37
Bar Pupko <Barpupco@gmail.com>
2022-08-29 09:43:59 +0300

    adding option to remove breakes between numbers

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function updateNumbers(){
     let userInput = document.getElementById("new-bio").value;
-    const re = RegExp('^[0-9]$');
-    console.log("regex: "+re.userInput);
+   
     var lines = userInput.split(/\r|\r\n|\n/);
     console.log("lines:"+lines);
     var count = lines.length;
@@ -11,14 +10,19 @@ function updateNumbers(){
     //creating elements with numbers
     lines.forEach(function(el) {
         var div = document.createElement("div1");
-        //regex code
-       
-        let link = `"https://api.whatsapp.com/send/?phone=%2B972${el}"\n`
+        let link = "";
+        el=el.replace('0','');//remove zeros
+        el= el.replace('-','');//remove break between number
+             if(el.includes('-')){//if there are another breake take it off too.
+                el=el.replace('-','');//remove break between number
+            }
+
+        link = `"https://wa.me/${el}"\n`//generate link
+        // link = `"https://api.whatsapp.com/send/?phone=%2B972${el}"\n`//generate link
         let blank = target ="_blank";
         let rel = "noreferrer noopener";
-        console.log(link);
-        console.log(el);
-      
+        console.log("lines: " +lines);
+        console.log("el: " +el);
         
         div.innerHTML = "\n<a href="+link + blank + rel +"a>"+el+"</a>\n";
         document.body.appendChild(div);
@@ -26,3 +30,4 @@ function updateNumbers(){
 
  
   };
+
